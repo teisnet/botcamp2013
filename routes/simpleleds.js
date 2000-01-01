a89826682e7ecc59b6968a9067f7b04e5380f5d0
Teis Draiby <teis@teis.net>
2000-01-01 01:32:54 +0000

    B Struggeling to get slides to work

--- routes/simpleleds.js
+++ routes/simpleleds.js
@@ -3,12 +3,14 @@
 var b = require('bonescript');
 //var io = require('socket.io');
 
+var ledsRangeValue = 3;
 var leds = {
     led1: {pin: "USR0", value: 0},
     led2: {pin: "USR1", value: 0},
     led3: {pin: "USR2", value: 0},
     led4: {pin: "USR3", value: 0}
     };
+    
 
 
 for (var key in leds ){
@@ -20,12 +22,18 @@ for (var key in leds ){
 
 
 exports.listener = function (socket) {
+    // SOCKET SETUP
+    
+    socket.emit('ledsRangeClient', ledsRangeValue);
+    
     for (var key in leds ){
         var led = leds[key];
         var ledValue = led.value == 1 ? "on" : "off";
         socket.emit('ledSwitch', { name: key, value: ledValue});
-    }    
+    }
     
+    
+    // ON LED SWITCH
   socket.on('ledSwitch', function (led) {
     console.log("ledSwitch: " + JSON.stringify(led));
     
@@ -35,6 +43,14 @@ exports.listener = function (socket) {
     
     socket.broadcast.emit('ledSwitch', led);
   });
+  
+  // ON LED RANGE
+  socket.on('ledsRange', function (value) {
+    ledsRangeValue = value;
+    console.log("ledsRange: " + value);
+    socket.broadcast.emit('ledsRangeClient', value);
+  });
+  
 };
 
 
